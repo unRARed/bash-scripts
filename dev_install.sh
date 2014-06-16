c8baaf48d03a43fa1d22eda11777fb77394df830
Ryan Richardson <ryananthonyrichardson@gmail.com>
2014-06-16 00:03:40 -0700

    Add fix for nvm and update version numbers

--- dev_install.sh
+++ dev_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Ubuntu 13.10 for nodeJS and Ruby development.
+# Ubuntu 14.04 for nodeJS and Ruby development.
 
 # Dependencies 
 sudo apt-get install python-software-properties curl -y
@@ -12,19 +12,19 @@ git config --global alias.history "log --color --graph --pretty=format:'%Cred%h%
 
 # NVM: node-version manager and node
 curl https://raw.githubusercontent.com/creationix/nvm/v0.5.1/install.sh | sh
-source ~/.nvm/nvm.sh
+echo "export NVM_DIR=~/.nvm" >> ~/.bashrc
 echo "source ~/.nvm/nvm.sh" >> ~/.bashrc
-nvm install v0.10.26
-nvm use v0.10.26
-nvm alias default 0.10
+nvm install v0.11.13
+nvm use v0.11.13
+nvm alias default 0.11
 
 # RVM: ruby-version manager, ruby and rails
 sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
 curl -L https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc # this doesn't always work
-rvm install 2.1.0
-rvm use 2.1.0 --default
+rvm install 2.1.2
+rvm use 2.1.2 --default
 
 # Heroku Toolbelt
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
