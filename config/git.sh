1aeda39945590992d3c3cf920aa9001e412ab139
Ryan Richardson <ryananthonyrichardson@gmail.com>
2017-05-08 14:17:53 -0700

    Fix and make executable Git config from linuxfest talk

--- config/git.sh
+++ config/git.sh
@@ -1,11 +1,11 @@
 ##!/usr/bin/env bash
 
-git config --global color.ui branch
-git config --global color.ui diff
-git config --global color.ui grep
-git config --global color.ui interactive
-git config --global color.ui showbranch
-git config --global color.ui status
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.grep auto
+git config --global color.interactive auto
+git config --global color.showbranch auto
+git config --global color.status auto
 git config --global color.ui auto
 git config --global merge.ff false
 git config --global pull.ff only
@@ -13,6 +13,6 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-git config --global alias.prune remote prune origin
-git config --global alias.nuke push origin --delete
+git config --global alias.prune "remote prune origin"
+git config --global alias.nuke "push origin --delete"
 git config --global alias.lg "log --graph --abbrev-commit --date=relative --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
