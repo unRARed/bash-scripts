833cf8922b3dd44e7c7326a65da61af23709505d
Ryan Richardson <unRARed@users.noreply.github.com>
2017-08-02 12:17:33 -0700

    Update git.sh

--- config/git.sh
+++ config/git.sh
@@ -15,6 +15,8 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.prune "remote prune origin"
 git config --global alias.nuke "push origin --delete"
+# :groom -> Removes all local branches that have been fully merged
+git config --global alias.groom "branch --merged master | grep -v 'master$' | xargs git branch -d"
 git config --global alias.lg "log --graph --abbrev-commit --date=relative --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 cp .git_pr.sh ~/
 echo 'source ~/.git_pr.sh' >> ~/.bashrc
