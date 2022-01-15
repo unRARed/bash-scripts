83e0a2c1718bd913575e92dff196ffa9996c956f
Ryan Richardson <ryananthonyrichardson@gmail.com>
2022-01-15 09:13:30 -0800

    Update _rvm.sh

--- shared/_rvm.sh
+++ shared/_rvm.sh
@@ -3,8 +3,9 @@
 read -r -p "Install Ruby Version Manager (rvm)? [y/N] " response
 if [[ "$response" =~ $YES ]]
 then
+  curl -sSL https://rvm.io/mpapis.asc | gpg --import -
   curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
-  curl -sSL https://get.rvm.io | bash -s stable
+  curl -L https://get.rvm.io | bash -s stable --auto-dotfiles
   echo 'alias rails_reset="RAILS_ENV=development rails db:drop db:create db:migrate db:seed"' >> ~/.zshrc
   source ~/.zshrc
   rvm install 2.7.2 --default
